5d761987315d62aba759bf291c0be6070649dcde
vivek48u <patilvivek2210@gmail.com>
2026-01-11 12:51:42 +0530

    added html file

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Render 🚀");
+  res.send("hello.html",{root:__filename});
 });
+
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server running");
