09286f1191439dc54e29cc53412a8cd655a7762b
vivek48u <patilvivek2210@gmail.com>
2026-01-11 13:05:41 +0530

    update server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-import express from "express";
+const express = require('express')
 
-const app = express();
+const app = express()
+const port = 3000
+
 
 app.get("/", (req, res) => {
-  res.send("hello.html",{root:__filename});
-});
+    
+    res.sendFile('hello.html',{root:__dirname})
+})
 
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server running");
-});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
